19be56461de33008760785a1e1bf2eab79e7d238
Hien Vu <vuxuanhien.2311@gmail.com>
2023-11-28 15:38:57 +0700

    update vxsoft uses and footer

--- src/app/shared/layout.component.ts
+++ src/app/shared/layout.component.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common'
 import { RouterOutlet } from '@angular/router'
 import { HeaderComponent } from './header/header.component'
 import { SecondHeaderComponent } from './second-header/second-header.component'
+import { FooterComponent } from './footer/footer.component'
 
 @Component({
   selector: 'vx-layout',
   standalone: true,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
-  imports: [CommonModule, RouterOutlet, HeaderComponent, SecondHeaderComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    SecondHeaderComponent,
+    FooterComponent,
+  ],
 })
 export class LayoutComponent {}
